87b1404bbd2a6f1b34b99f30f0daad33439bc6fa
tebogoditse <tebogoditse2@gmail.com>
2024-11-22 00:03:58 +0200

    4-mutations.js

--- 0x06-React_Immutable/3-list.js
+++ 0x06-React_Immutable/3-list.js
@@ -1,8 +1,7 @@
 import { List } from 'immutable';
 
-export function getListObject(array) {
-  return List(array);
-}
-export function addElementToList(list, element) {
-  return list.push(element);
-}
+const getListObject = (array) => List(array);
+
+const addElementToList = (list, element) => list.push(element);
+
+export { getListObject, addElementToList };

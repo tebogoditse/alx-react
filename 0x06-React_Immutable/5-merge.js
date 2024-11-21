3d34b238579683a48d6efadd86c8c8618f59085b
tebogoditse <tebogoditse2@gmail.com>
2024-11-21 23:47:39 +0200

    5-merge.js

---
+++ 0x06-React_Immutable/5-merge.js
@@ -0,0 +1,7 @@
+import { List, Map } from 'immutable';
+
+const concatElements = (page1, page2) => List(page1).concat(List(page2));
+
+const mergeElements = (page1, page2) => Map(page1).merge(Map(page2));
+
+export { concatElements, mergeElements };

fce2d0b905ac686360f53a7ad2908ed11d0ecd73
tebogoditse <tebogoditse2@gmail.com>
2025-01-27 17:14:11 +0200

    fix webconfig file

--- 0x00-Webpack/task_2/webpack.config.js
+++ 0x00-Webpack/task_2/webpack.config.js
@@ -16,6 +16,16 @@ module.exports = {
       {
         test: /\.(png|svg|jpg|jpeg|gif)$/i,
         type: 'asset/resource',
+        use: [
+          "file-loader",
+          {
+            loader: "image-webpack-loader",
+            options: {
+              bypassOnDebug: true,
+              disable: true,
+            },
+          },
+        ]
       },
      ],
   },
